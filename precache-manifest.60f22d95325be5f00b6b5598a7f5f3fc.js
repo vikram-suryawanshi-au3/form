3b1e6568504b97e9176d704e166bf1f933b069e7
vikram <vikramsuryawanshi619.vs@gmail.com>
2020-10-31 17:48:35 +0530

    Updates

---
+++ precache-manifest.60f22d95325be5f00b6b5598a7f5f3fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c5562f882afd1eb6dd4c6783e824866",
+    "url": "/form/index.html"
+  },
+  {
+    "revision": "2e9e67313fb69f8cf190",
+    "url": "/form/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b9f2af45efe21a278099",
+    "url": "/form/static/js/2.3fbab7d0.chunk.js"
+  },
+  {
+    "revision": "25ee36cecf3742d8e72fa8504c054097",
+    "url": "/form/static/js/2.3fbab7d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e9e67313fb69f8cf190",
+    "url": "/form/static/js/main.467969fb.chunk.js"
+  },
+  {
+    "revision": "c22b8acaf4fb9d0dbbaa",
+    "url": "/form/static/js/runtime-main.fb106a53.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/form/static/media/logo.5d5d9eef.svg"
+  }
+]);
